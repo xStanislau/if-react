beddc9f0f8b3ea664ae47c58191f0fe1aa0ac0fc
StanislauHancharou <stanislau.hancharou@effective-soft.com>
2021-04-12 20:58:16 +0300

    add button component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from "./Button/Button"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button text="текст" />
+      <Button text="Другой текст" />
+      <Button text={1} />
+      <Button text={true} />
     </div>
   );
 }
---
+++ src/Button/Button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Button({ text }) {
+
+  return <button>{text}</button>
+}
+
+export default Button;
